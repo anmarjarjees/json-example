94470f7b1fe2f663bfc413c8dcf5641e8f7fe5d3
Anmar Jarjees <anmarjarjees@users.noreply.github.com>
2020-03-26 23:48:12 -0400

    Add files via upload

---
+++ music-inst.js
@@ -0,0 +1,27 @@
+[{
+    "name": "Piano",
+    "history": "very old",
+    "types": {
+        "Grand": ["Baby Grand", "Parlor Grand", "Concert Grand"],
+        "Upright": ["Acoustic", "Digital", "Hybrid"]
+    }
+},
+{
+    "name": "Guitar",
+    "history": "old",
+    " types": {
+        "Acoustic": ["six-string model ", " twelve-string models "],
+        "Eclectic": ["six-string model ", " twelve-string models "],
+        "Classical": ["six-string model ", " twelve-string models "]
+    }
+},
+{
+    "name": "Ukulele",
+    " history ": "new",
+    " types ": {
+        "Soprano": ["12 Frets", "13 Frets", "14 Frets"],
+        "Concert": ["15 Frets", "16 Frets", "17 Frets"],
+        "Tenor": ["17 Frets", "18 Frets", "19 Frets"]
+    }
+}
+]
